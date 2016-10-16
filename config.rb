664a5f0b08b051976461be9c998e4b71b16be4df
mikunpham <mikun1993@gmail.com>
2016-10-16 15:29:26 +0700

    Add all other files

---
+++ config.rb
@@ -0,0 +1,9 @@
+require 'compass/import-once/activate'
+http_path = "/"
+css_dir = "/public/css/"
+sass_dir = "/sass/"
+images_dir = "/public/img/"
+javascripts_dir = "/public/js/"
+line_comments = false
+output_style = :compressed
+
